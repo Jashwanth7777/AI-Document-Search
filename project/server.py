49180e22a0d7c2e44448eda4ba4645558b632ff0
jashwanth7 <jashwanthgp88@gmail.com>
2025-06-15 10:32:48 +0530

    server port update

--- project/server.py
+++ project/server.py
@@ -1,4 +1,6 @@
+import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=10000, reload=True)
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT is not set
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
